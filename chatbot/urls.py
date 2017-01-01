60d0a84c6e94ebf134b8d0b02fc1e3ce50bed3f2
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-01-02 01:09:36 +0530

    add url for CBV

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+from chatbotapp.views import ChatBotView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', ChatBotView.as_view())
 ]
